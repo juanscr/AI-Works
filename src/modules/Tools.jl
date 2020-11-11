8130b7691e41e54cb11eecc5676be1e8e69df6be
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2020-11-11 11:32:43 -0500

    [ src ] Added method to clean indexes.

--- src/modules/Tools.jl
+++ src/modules/Tools.jl
@@ -1,3 +1,10 @@
+# Normalize data
 function normalize(data)
     return data ./ maximum(data, dims = 1)
+end
+
+# Clean indexes
+function purge(indexes :: Vector{Int64})
+    index_neg = findfirst(x -> x < 0, indexes)
+    return indexes[1:(index_neg - 1)]
 end
