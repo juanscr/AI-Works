475ab95acea5db6deb91d3a02de637cb6a885b59
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2020-11-15 12:00:02 -0500

    [ src ] Completed SVM result generation:

--- src/SVM.jl
+++ src/SVM.jl
@@ -5,8 +5,23 @@ include("modules/Tools.jl")
 @sk_import svm: SVC
 
 # ==== Functions ==== #
+function generate_meshgrid(data_name :: String, name_file :: String; n = 10)
+    datax, _ = create_data(data_name, sep = false)
+    mesh = meshgrid(datax, n)
+    open(name_file, "w") do io
+        write(io, "Meshgrid\n")
+        for i in 1:size(mesh, 1)
+            for j in 1:size(mesh, 2)
+                write(io, string(mesh[i, j], ","))
+            end
+            write(io, "\n")
+        end
+    end
+    return mesh
+end
+
 function generate_results(svm, name_file :: String, data_name :: String,
-                          kernel :: String, n = 10)
+                          kernel :: String, mesh :: Matrix{Float64}; n = 5)
     datax, _ = create_data(data_name, sep = false)
     class = svm.predict(datax)
 
@@ -21,8 +36,11 @@ function generate_results(svm, name_file :: String, data_name :: String,
         end
 
         # Meshgrid
-        mesh = meshgrid(datax, n)
         Z = decision_function(svm, mesh)
+        write(io, "Meshgrid\n")
+        for i in 1:length(Z)
+            write(io, string(Z[i], "\n"))
+        end
     end
 end
 
@@ -34,7 +52,17 @@ train_datax = train_data[1]
 train_datay = train_data[2]
 
 # SVM
-svm = SVC(kernel = "linear")
-fit!(svm, train_datax, reshape(train_datay, :))
-generate_results(svm, "../results/svm-results.csv", "data/num-data.csv",
-                 "Linear")
+kernels = ["linear", "rbf", "poly"]
+
+# Files
+data_name = "data/num-dats.csv"
+meshgrid_file = "../results/meshgrid.csv"
+svm_results = "../results/svm-results.csv"
+
+# Output
+mesh = generate_meshgrid(data_name, meshgrid_file, n = 5)
+for kernel in kernels
+    svm = SVC(kernel = kernel)
+    fit!(svm, train_datax, reshape(train_datay, :))
+    generate_results(svm, svm_results, data_name, kernel, mesh)
+end
