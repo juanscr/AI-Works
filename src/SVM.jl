106b87e764f7d2193cce8e48a31f07eb5df5f853
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2020-11-16 21:11:54 -0500

    [ src ] Exported joblib file.

--- src/SVM.jl
+++ src/SVM.jl
@@ -1,28 +1,10 @@
 # ==== Global behavior ==== #
+using PyCall
 using ScikitLearn
 include("modules/Tools.jl")
 
+@pyimport joblib
 @sk_import svm: SVC
-
-# ==== Functions ==== #
-function generate_meshgrid(data_name :: String, name_file :: String; n = 10)
-    datax, _ = create_data(data_name, sep = false)
-    mesh = meshgrid(datax, n)
-    open(name_file, "w") do io
-        write(io, "X1,X2,X3,X4,X5,X6\n")
-        for i in 1:size(mesh, 1)
-            for j in 1:size(mesh, 2)
-                if j == size(mesh, 2)
-                    write(io, string(mesh[i, j], "\n"))
-                else
-                    write(io, string(mesh[i, j], ","))
-                end
-            end
-        end
-    end
-    return mesh
-end
-
 
 # ==== Main ==== #
 # Data
@@ -40,10 +22,10 @@ meshgrid_file = "../results/meshgrid.csv"
 svm_results = "../results/svm-results.csv"
 
 # Output
-mesh = generate_meshgrid(data_name, meshgrid_file, n = 5)
+prefix = "../results/svm-"
 for kernel in kernels
     svm = SVC(kernel = kernel)
     fit!(svm, train_datax, reshape(train_datay, :))
-    extra_info = string("Kernel,", kernel)
-    generate_results(svm, svm_results, data_name, mesh, extra_info=extra_info)
+    joblib.dump(svm, string(prefix, kernel, ".joblib"))
+
 end
