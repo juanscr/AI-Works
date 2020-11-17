5207c8906475f0489977395c7ef813faa607f1bb
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2020-11-16 21:12:11 -0500

    [ src ] Removed unused function.

--- src/Tree.jl
+++ src/Tree.jl
@@ -7,22 +7,6 @@ include("modules/Tools.jl")
 @pyimport joblib
 @sk_import tree: DecisionTreeClassifier
 
-# ==== Function ==== #
-function read_meshgrid(name_file :: String)
-    rows = CSV.Rows(name_file)
-
-    # Mesh
-    mesh = []
-    for row in rows
-        row_data = zeros(6)
-        for i in 1:6
-            row_data[i] = parse(Float64, row[i])
-        end
-        push!(mesh, row_data)
-    end
-    return collect(transpose(hcat(mesh...)))
-end
-
 # ==== Main ==== #
 # Files
 data_name = "data/num-dats.csv"
@@ -36,7 +20,6 @@ train_datay = train_data[2]
 
 # Decision tree
 dt = DecisionTreeClassifier(criterion = "entropy")
-mesh = read_meshgrid("../results/meshgrid.csv")
 fit!(dt, train_datax, reshape(train_datay, :))
 
 # Save tree
