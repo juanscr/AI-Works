b6a4ce8e5854a225b28299fb5668176170c943aa
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2020-11-16 21:41:53 -0500

    [ src ] Added other dimension compliance.

--- src/modules/Tools.jl
+++ src/modules/Tools.jl
@@ -14,25 +14,17 @@ end
 # Get data
 function create_data(name_file :: String; sep = true, norm = true)
     # Data reading
-    data_csv = CSV.File("data/num-data.csv")
-    data = zeros(length(data_csv), 7)
-    k = 1
-    for row in data_csv
-        data[k, 1] = row.Column1
-        data[k, 2] = row.Column2
-        data[k, 3] = row.Column3
-        data[k, 4] = row.Column4
-        data[k, 5] = row.Column5
-        data[k, 6] = row.Column6
-        data[k, 7] = row.Column7
-        k += 1
+    data_col = CSV.File(name_file).columns
+    data = zeros(length(data_csv), length(columns))
+    for i in 1:length(columns)
+        data[:, i] = data_col[i]
     end
     if norm
         data = normalize(data)
     end
 
     if !sep
-        return data[:, 1:6], data[:, 7:end]
+        return data[:, 1:(end - 1)], data[:, end:end]
     end
 
     # Separate data
@@ -42,14 +34,14 @@ function create_data(name_file :: String; sep = true, norm = true)
     indexes_val = purge(trunc.(Int, indexes_csv.columns[3]))
 
     # Create data
-    train_datax = data[indexes_tr, 1:6]
-    train_datay = data[indexes_tr, 7:end]
+    train_datax = data[indexes_tr, 1:(end - 1)]
+    train_datay = data[indexes_tr, end:end]
 
-    test_datax = data[indexes_te, 1:6]
-    test_datay = data[indexes_te, 7:end]
+    test_datax = data[indexes_te, 1:(end - 1)]
+    test_datay = data[indexes_te, end:end]
 
-    val_datax = data[indexes_val, 1:6]
-    val_datay = data[indexes_val, 7:end]
+    val_datax = data[indexes_val, 1:(end - 1)]
+    val_datay = data[indexes_val, end:end]
 
     return (train_datax, train_datay), (test_datax, test_datay),
            (val_datax, val_datay)
