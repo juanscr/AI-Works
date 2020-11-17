4bc32e6bf1a83a322e2a438b186f82317e15c48b
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2020-11-16 23:55:34 -0500

    [ src ] Exported embedded svms.

--- src/SVM.jl
+++ src/SVM.jl
@@ -8,16 +8,13 @@ include("modules/Tools.jl")
 
 # ==== Main ==== #
 # Data
-train_data, _, _ = create_data("data/num-data.csv")
-
+data_name = "data/num-data.csv"
+train_data, _, _ = create_data(data_name)
 train_datax = train_data[1]
 train_datay = train_data[2]
 
 # SVM
 kernels = ["linear", "rbf", "poly"]
-
-# Files
-data_name = "data/num-data.csv"
 
 # Output
 prefix = "../results/svm-"
@@ -29,11 +26,14 @@ end
 
 # Files embedded data
 data_name = "data/embedded-data.csv"
+train_datae, _, _ = create_data(data_name)
+train_dataxe = train_datae[1]
+train_dataye = train_datae[2]
 
 # Output
 prefix = "../results/svm-emb-"
 for kernel in kernels
     svm = SVC(kernel = kernel)
-    fit!(svm, train_datax, reshape(train_datay, :))
+    fit!(svm, train_dataxe, reshape(train_dataye, :))
     joblib.dump(svm, string(prefix, kernel, ".joblib"))
 end
