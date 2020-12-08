a107c2c81793ffe7ac60216bebfd0b264d3a3e7a
Juan Sebastian Cardenas <jcardenasr123@gmail.com>
2020-12-08 13:50:47 -0500

    [ HM ] Added data extraction of lenet5.

--- hybrid-models/src/Lenet5.jl
+++ hybrid-models/src/Lenet5.jl
@@ -1,29 +1,98 @@
 # ========== Packages ========== #
+using BSON: @save, @load
 using CUDAapi
 using Flux
 using MLDatasets: MNIST
 using Plots
+using Statistics
 
 import ProgressMeter
 
 # ========== Functions ========== #
+function get_image(input_case)
+    return reverse(input_case[:, :, 1], dims=2)'
+end
+
 function write_file(xtest, ytest, trained_model)
+    open("../results/lenet5.csv", "w") do io
+        # Real values
+        write(io, "Real\n")
+        for y in ytest
+            write(io, string(y, "\n"))
+        end
+
+        # Trained model output
+        write(io, "Model output\n")
+        for i in 1:size(xtest)[4]
+            input = xtest[:, :, :, i]
+            y_model = trained_model(input)
+
+            # Printing
+            for y in y_model
+                write(io, string(y, ","))
+            end
+            write(io, "\n")
+        end
+    end
 end
 
 function extract_activation(input, trained_model, layers)
-    activation_spaces = []
-    for layer in layers
-        push!(activation_spaces, trained_model[1:layer](input))
-    end
+    # X-test graph
+    input_cpu = input |> cpu
+    heatmap(get_image(input_cpu[:, :, 1]), color=:greys)
+    savefig("../article/figs/input-act-space.pdf")
 
-    # Save image
-    for act_space in activation_spaces
+    # Plotting activation spaces
+    for i in 1:length(layers)
+        layer = layers[i]
+        act_space = trained_model[1:layer](input) |> cpu
+
+        mean_act = mean(act_space, dims = 3)
+        heatmap(get_image(mean_act[:, :, 1]), color=:greys)
+        savefig(string("../article/figs/act-space-", i, ".pdf"))
     end
 end
 
 function extract_model_info(xtest, ytest, trained_model, layers)
-    input = xtest[:, :, :, 1] |> gpu
+    input = xtest[:, :, :, 2] |> gpu
     extract_activation(input, trained_model, layers)
+
+    # Write file
+    trained_model_cpu = trained_model |> cpu
+    write_file(xtest, ytest, trained_model_cpu)
+end
+
+function train(lenet5, train_loader; η=3e-4, epocs=200, create=true)
+    if !create
+        @load "lenet5.bson" model
+        return model |> gpu
+    end
+
+    # Cuda model
+    model = lenet5 |> gpu
+
+    # Parameters
+    ps = Flux.params(model)
+    opt = Flux.Optimise.Descent(η)
+    loss(y_appr, y) = Flux.Losses.mse(y_appr, y)
+
+    # Training
+    p = ProgressMeter.Progress(epocs)
+    for epoc in 1:epocs
+        for (x, y) in train_loader
+            x, y = x |> gpu, y |> gpu
+            gs = Flux.gradient(ps) do
+                y_appr = model(x)
+                loss(y_appr, y)
+            end
+            Flux.Optimise.update!(opt, ps, gs)
+        end
+        ProgressMeter.next!(p)
+    end
+
+    model_cpu = model |> cpu
+    @save "lenet5.bson" model_cpu
+    return model
 end
 
 # ========== Parameters ========== #
@@ -60,29 +129,8 @@ aux_reshape2 = x -> reshape(x, :, size(x, 4))
 lenet5 = Chain(aux_reshape1, C1, S2, C3, S4, aux_reshape2, C5, F6, out)
 
 # ========== Training ========== #
-# Cuda model
-model = lenet5 |> gpu
-
-# Parameters
-ps = Flux.params(lenet5)
-opt = Flux.Optimise.Descent(3e-4)
-loss(y_appr, y) = Flux.Losses.mse(y_appr, y)
-epocs = 5
-
-# Training
-for epoc in 1:epocs
-    p = ProgressMeter.Progress(length(train_loader))
-
-    for (x, y) in train_loader
-        x, y = x |> gpu, y |> gpu
-        gs = Flux.gradient(ps) do
-            y_appr = model(x)
-            loss(y_appr, y)
-        end
-        Flux.Optimise.update!(opt, ps, gs)
-        ProgressMeter.next!(p)
-    end
-end
+create = true
+model = train(lenet5, train_loader, η=0.01, epocs=200, create=create)
 
 # ========== Exporting info ========== #
 # Testing data
